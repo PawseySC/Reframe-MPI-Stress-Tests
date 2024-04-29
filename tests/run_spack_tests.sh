18fea363077058d7a9ba753473743ec1ac7264c9
craigmeyer <craig.meyer9112@gmail.com>
2024-04-29 16:10:14 +0800

    Update to bash scripts for running tests

--- tests/run_spack_tests.sh
+++ tests/run_spack_tests.sh
@@ -3,19 +3,58 @@
 repo_root_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 rfm_settings_file=${repo_root_dir}/setup_files/settings.py
 
-# All packages are in one spack build environment
-if [ -z "$1" ]
+# Define help message to be printed
+usage() {
+    declare -r script_name=$(basename "$0")
+    echo """
+Usage:
+"${script_name}" [option] <name>
+
+Option:
+    [-e]: optional list of spack build environments to run the reframe tests over
+    [-o]: optional reframe command-line arguments one wishes to use
+"""
+}
+
+# Default value of paossible passed arguments
+reframe_opts=""
+env_list=""
+
+# Process command-line arguments
+while getopts ':e:o:' opt; do
+  case "${opt}" in
+    e)
+        env_list=${OPTARG}
+        ;;
+    o)
+        # Convert comma-separated string of options to array of option strings
+        options=(${OPTARG//,/ })
+        # Convert array of option strings to one string of options separated by whitespace ' '
+        for i in ${options[@]}
+        do
+            reframe_opts="${reframe_opts} ${i}"
+        done
+        echo "Running spack tests with the following reframe options: $reframe_opts"
+        ;;
+    h | *)
+      usage
+      exit 0
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+# Call Reframe according to passed arguments
+if [ -z "$env_list" ]
 then
-  printf "Running spack tests with a single spack build environment\n\n"
-  reframe -C ${rfm_settings_file} -c ${repo_root_dir}/tests/spack/spack_checks.py -l -v
-# Packages are split across multiple build environments
+    printf "Running spack tests with a single build environemnt\n\n"
+    echo reframe -C ${rfm_settings_file} -c ${repo_root_dir}/tests/spack/spack_checks.py -r $reframe_opts
 else
-  env_list=$1
-  printf "Running spack tests for each of the following spack build environments: $1\n\n"
-  for env in $env_list; do
-    export SPACK_ENV=${env}
-    printf "Running spack tests for each of the following spack build environments: $env\n\n"
-    reframe -C ${rfm_settings_file} -c ${repo_root_dir}/tests/spack/spack_checks.py -l -v
-    unset SPACK_ENV
-  done
+    printf "Running spack tests for each of the following spack build environments: $env_list\n\n"
+    for env in $env_list; do
+        export SPACK_ENV=${env}
+        printf "Running spack tests for spack build environment $env\n\n"
+        echo reframe -C ${rfm_settings_file} -c ${repo_root_dir}/tests/spack/spack_checks.py -r $reframe_opts
+        unset SPACK_ENV
+    done
 fi
